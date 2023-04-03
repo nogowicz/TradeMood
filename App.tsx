e856a8cca47956aec40f5e19fa303da2843c7136
Nogowicz <nogovitz00@gmail.com>
2023-04-03 17:17:32 +0200

    Restricting Dashboard

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { StatusBar } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen';
 
 import LangProvider, { LangModeProvider } from './src/lang/LangProvider';
-import { FormattedMessage } from 'react-intl';
-import { Text } from 'react-native';
 import Overview from './views/authenticated/overview/Overview';
+import { colors } from './src/styles';
 
 
 
 
 function App() {
+
+  const Stack = createNativeStackNavigator();
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
-    <LangModeProvider>
-      <LangProvider>
-        <Overview />
-      </LangProvider>
-    </LangModeProvider>
+    <>
+      <StatusBar
+        backgroundColor={colors.LIGHT_COLORS.BACKGROUND}
+        barStyle='dark-content'
+      />
+      <LangModeProvider>
+        <LangProvider>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen
+                name='Overview'
+                component={Overview}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </LangProvider>
+      </LangModeProvider>
+    </>
   );
 }
 
--- android/app/src/main/java/com/trademood/SplashActivity.java
+++ android/app/src/main/java/com/trademood/SplashActivity.java
@@ -1,2 +1,17 @@
-package com.trademood;public class SplashActivity {
+package com.trademood;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
