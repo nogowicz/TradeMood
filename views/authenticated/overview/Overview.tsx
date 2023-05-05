79c9c824630fc3d6bda5713ae6360724cdc89c7f
Nogowicz <nogovitz00@gmail.com>
2023-05-05 18:25:40 +0200

    Created Trending now widget

--- views/authenticated/overview/Overview.tsx
+++ views/authenticated/overview/Overview.tsx
@@ -12,11 +12,12 @@ import { RootStackParamList } from '../../navigation/Navigation';
 import { AuthContext } from '@views/navigation/AuthProvider';
 import { colors, spacing, typography } from 'styles';
 import ProfileBar from 'components/profile-bar';
-import IconButton from 'components/buttons/icon-button/IconButton';
+import IconButton from 'components/buttons/icon-button';
 
 import Bell from 'assets/icons/Bell-icon.svg'
 import Search from 'assets/icons/Search.svg'
 import { SCREENS } from '@views/navigation/constants';
+import TrendingNow from 'components/trending-now';
 
 
 type OverviewScreenNavigationProp = NativeStackScreenProps<RootStackParamList, 'Overview'>;
@@ -62,6 +63,15 @@ export default function Overview({ navigation }: OverviewProps) {
                             id='views.home.overview-title'
                         />
                     </Text>
+                </View>
+                <View>
+                    <TrendingNow
+                        name='Bitcoin'
+                        positive={60}
+                        neutral={10}
+                        negative={30}
+                        onPress={() => console.log("Navigating to details screen")}
+                    />
                 </View>
 
             </View>
