94127bcca7d79bfccc8fa3466747f523fb5a273f
Nogowicz <nogovitz00@gmail.com>
2023-10-31 16:51:53 +0100

    about me text field in progress

--- views/authenticated/sub-views/profile-wall/UserInfo.tsx
+++ views/authenticated/sub-views/profile-wall/UserInfo.tsx
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useLayoutEffect, useState } from 'react'
 import { useTheme } from 'store/ThemeContext'
 import { useAuth } from 'store/AuthProvider';
 import CustomImage from 'components/custom-image';
 import { constants, spacing, typography } from 'styles';
 import firestore from '@react-native-firebase/firestore';
+import { useNavigation } from '@react-navigation/native';
+import { SCREENS } from '@views/navigation/constants';
+import { useIntl } from 'react-intl';
 
 type UserInfoProps = {
     userUID?: string;
@@ -15,6 +18,14 @@ export default function UserInfo({ userUID }: UserInfoProps) {
     const { user } = useAuth();
     const [photoURL, setPhotoURL] = useState();
     const [displayName, setDisplayName] = useState();
+    const navigation = useNavigation();
+    const isMyProfile = (user && (user.uid === userUID) || userUID === undefined) ? true : false;
+    const intl = useIntl();
+    //translations:
+    const aboutMeTranslation = intl.formatMessage({
+        id: 'views.home.profile-wall.about-me.placeholder',
+        defaultMessage: 'Tell something about yourself'
+    });
 
     useLayoutEffect(() => {
         async function getUserDetails() {
@@ -49,16 +60,33 @@ export default function UserInfo({ userUID }: UserInfoProps) {
                         ...styles.profileImage
                     }}
                 /> :
-                <CustomImage
-                    url={user.photoURL}
-                    style={{
-                        ...styles.profileImage
-                    }}
-                />}
+                <TouchableOpacity
+                    activeOpacity={constants.ACTIVE_OPACITY.HIGH}
+                    onPress={() => navigation.navigate(SCREENS.HOME.EDIT_PICTURE.ID as never)}
+                >
+                    <CustomImage
+                        url={user.photoURL}
+                        style={{
+                            ...styles.profileImage
+                        }}
+                    />
+                </TouchableOpacity>}
             <Text style={{
                 ...styles.nameText,
                 color: theme.TERTIARY,
             }}>{userUID ? displayName : user.displayName}</Text>
+
+            <TextInput
+                style={[{
+                    ...styles.aboutMeText,
+                    borderWidth: isMyProfile ? 1 : 0,
+                    borderColor: theme.LIGHT_HINT,
+                }]}
+                placeholder={aboutMeTranslation}
+                multiline={true}
+                editable={isMyProfile}
+                maxLength={280}
+            />
         </View>
     );
 }
@@ -79,4 +107,11 @@ const styles = StyleSheet.create({
         fontWeight: typography.FONT_WEIGHT_BOLD,
         textAlign: 'center',
     },
+    aboutMeText: {
+        width: '100%',
+        marginVertical: spacing.SCALE_20,
+        borderRadius: constants.BORDER_RADIUS.INPUT,
+        paddingHorizontal: spacing.SCALE_20,
+        justifyContent: 'center',
+    }
 })
