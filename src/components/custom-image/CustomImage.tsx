78c433c45b389794572917f7c1506e04f9696565
Nogowicz <nogovitz00@gmail.com>
2023-11-08 15:03:16 +0100

    resizing image when uploading to firestore

--- src/components/custom-image/CustomImage.tsx
+++ src/components/custom-image/CustomImage.tsx
@@ -1,4 +1,4 @@
-import { StyleProp, View, ImageSourcePropType, ImageURISource, Image, ImageStyle } from 'react-native';
+import { StyleProp, View, ImageSourcePropType, Image, ImageStyle, Text } from 'react-native';
 import React, { useState } from 'react';
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 
@@ -10,33 +10,47 @@ type ImageProps = {
 };
 
 export default function CustomImage({ url, source, style, size }: ImageProps) {
-    const [isImageDownloading, setIsImageDownloading] = useState(false);
+    const [isImageDownloading, setIsImageDownloading] = useState(true);
+    const [isImageLoadError, setIsImageLoadError] = useState(false);
 
-    const handleImageLoad = () => {
-        setIsImageDownloading(!isImageDownloading);
+    const handleImageLoadStart = () => {
+        setIsImageDownloading(true);
+        setIsImageLoadError(false);
+    };
+
+    const handleImageLoadEnd = () => {
+        setIsImageDownloading(false);
+    };
+
+    const handleImageError = () => {
+        setIsImageDownloading(false);
+        setIsImageLoadError(true);
     };
 
     return (
         <>
-            {(url || source) && isImageDownloading && (
+            {(url || source) && isImageDownloading && !isImageLoadError && (
                 <SkeletonPlaceholder>
                     <View style={style} />
                 </SkeletonPlaceholder>
             )}
-            <Image
-                source={url ? { uri: url } : source}
-                style={[
-                    {
-                        height: size,
-                        width: size,
-                        borderRadius: size && size / 2,
-                        display: (url || source) && isImageDownloading ? 'none' : 'flex',
-                    },
-                    style,
-                ]}
-                onLoadStart={handleImageLoad}
-                onLoadEnd={handleImageLoad}
-            />
+            {(
+                <Image
+                    source={url ? { uri: url } : source}
+                    style={[
+                        {
+                            height: size,
+                            width: size,
+                            borderRadius: size && size / 2,
+                            display: (url || source) && isImageDownloading ? 'none' : 'flex',
+                        },
+                        style,
+                    ]}
+                    onLoadStart={handleImageLoadStart}
+                    onLoadEnd={handleImageLoadEnd}
+                    onError={handleImageError}
+                />
+            )}
         </>
     );
 }
